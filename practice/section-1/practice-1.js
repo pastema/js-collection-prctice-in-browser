153ecd7ddbe9841d80a507cfa6fcfde8206618ab
Martin Pastera <martin.lester.pastera@oocl.com>
2019-09-12 17:52:59 +0800

    finished practice 1 and 2 section 1

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,22 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  var myArray = new Array();
+  
+  for(var x = 0; x <= collectionA.length-1; x++)
+  {
+      for(var y = 0; y <=collectionB.length-1; y++)
+      {
+        if(collectionB[y] == collectionA[x])
+        {
+          myArray.push(collectionB[y]);
+        }
+        else
+        {
+         
+        }
+      }
+  }
+
+  return myArray;
 }
--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,21 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  var myArray = new Array();
+  
+  for(var x = 0; x <= collectionA.length-1; x++)
+  {
+      for(var y = 0; y <=collectionB[0].length-1; y++)
+      {
+        if(collectionB[0][y] == collectionA[x])
+        {
+          myArray.push(collectionB[0][y]);
+        }
+        else
+        {
+         
+        }
+      }
+  }
+  return myArray;
 }

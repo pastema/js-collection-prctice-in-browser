b0ea814fe63d2c7a8fdb2d82829a958617ae737e
MartinPastera <36395393+MartinPastera@users.noreply.github.com>
2019-09-14 02:10:44 +0800

    test

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -13,21 +13,21 @@ var getLetters = new Array();
 function countSameElements(collection) {
   var getCollectedLetters = new Array();
   
-  for(var x=0; x<=collection.length-1;x++) //9
+  for(var x=0; x<=collection.length-1;x++)
   {
     if(getLetters[0] != collection[x])   
     {
       if(getLetters.length != 0 )     
       { 
-        getCollectedLetters = getLetters;     /// aaa = aaa
-        final.push(getLettersList(getCollectedLetters)); //show key,count
-        getLetters.length = 0;    //empty array
+        getCollectedLetters = getLetters;    
+        final.push(getLettersList(getCollectedLetters)); 
+        getLetters.length = 0;    
       }
     }
-      getLetters.push(collection[x]); //eeeeeee
+      getLetters.push(collection[x]); 
       if(x+1 == collection.length)
       {
-        final.push(getLettersList(getCollectedLetters)); //show key,count
+        final.push(getLettersList(getCollectedLetters)); 
       }
 }
      return(final);

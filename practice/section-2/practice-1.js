ac3392879d7df8f67473caae960b216de6c5d5d5
MartinPastera <36395393+MartinPastera@users.noreply.github.com>
2019-09-14 01:50:22 +0800

    finished section 2 practice 1

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,35 @@
 'use strict';
+var final = new Array();
+var getLetters = new Array();
+ function getLettersList(collection) {
+   var letter = collection[0];
+  return {
+    key: letter,
+    count: collection.length
+  }; 
+} 
+
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+  var getCollectedLetters = new Array();
+  
+  for(var x=0; x<=collection.length-1;x++) //9
+  {
+    if(getLetters[0] != collection[x])   
+    {
+      if(getLetters.length != 0 )     
+      { 
+        getCollectedLetters = getLetters;     /// aaa = aaa
+        final.push(getLettersList(getCollectedLetters)); //show key,count
+        getLetters.length = 0;    //empty array
+      }
+    }
+      getLetters.push(collection[x]); //eeeeeee
+      if(x+1 == collection.length)
+      {
+        final.push(getLettersList(getCollectedLetters)); //show key,count
+      }
+}
+     return(final);
+
 }

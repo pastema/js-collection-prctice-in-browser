2c4dbc041dbf779fdb6d07104f674cf8b85d832d
Martin Pastera <martin.lester.pastera@oocl.com>
2019-09-13 17:04:52 +0800

    finished practice 1 section 3

--- practice/section-3/practice-1.js
+++ practice/section-3/practice-1.js
@@ -1,5 +1,45 @@
 'use strict';
 
+
+function getcollectsame(key, count) {
+  return {
+    key: key,
+    count: count - 1
+  };
+}
+
+function getcollect(key, count) {
+  return {
+    key: key,
+    count: count
+  };
+}
+
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+
+  var myArray = new Array();
+  var ifSame = 0;
+  
+for(var x = 0; x <= collectionA.length-1; x++)
+{
+  ifSame = 0;
+  for(var y = 0; y <=objectB.value.length-1; y++) 
+  { 
+    if(collectionA[x].key == objectB.value[y])    
+    {
+      myArray.push(getcollectsame(collectionA[x].key,collectionA[x].count));  
+      ifSame = 1;
+    }
+    else
+    {
+      
+    }
+  }
+    if(ifSame == 0)
+    { 
+      myArray.push(getcollect(collectionA[x].key,collectionA[x].count));
+    }
+}
+
+return myArray;
 }
